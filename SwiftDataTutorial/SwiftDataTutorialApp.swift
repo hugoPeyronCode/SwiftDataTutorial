787da5d29f97b971406990fe72d33080a84c7bf5
Hugo Peyron <hugopeyron@gmail.com>
2024-08-31 19:55:02 +0200

    Initial Commit

---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Hugo Peyron on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
